696d9f6e68f2f53e4a4634317b4bff3fcefe4a32
Spencer Pearlman <spencerj521@gmail.com>
2019-08-06 14:22:21 -0400

    Set up command line arguemnt variables

--- Harpocrates/Harpocrates/CommandLineArg/CLIArgs.cs
+++ Harpocrates/Harpocrates/CommandLineArg/CLIArgs.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using MatthiWare.CommandLine.Core.Attributes;
 
 namespace Harpocrates.CommandLineArg
 {
     public class CLIArgs
     {
-        public CLIArgs()
-        {
-        }
+        [Required, Name("first", "first_name"), Description("First name of target")]
+        public string FirstName { get; set; }
+        [Required, Name("last", "last_name"), Description("Last name of target")]
+        public string LastName { get; set; }
+        [Name("birth_date", "birth_date"), Description("Birthday of target")]
+        public string Birthday { get; set; }
+        [Name("uanme", "username"), Description("Input a known username of the target")]
+        public string Username { get; set; }
+        [Name("l", "location"), Description("Where the target is from")]
+        public string Location { get; set; }
+        [Name("c", "company"), Description("Target's job")]
+        public string Company { get; set; }
+        [Name("p", "profile"), Description("If you have previously done some recon, you may attach an excel or json formatted document")]
+        public string JSONProfile { get; set; }
+        [Name("v", "verbose"), Description("Verbose Output"), DefaultValue(true)]
+        public bool Verbose { get; set; }
     }
 }

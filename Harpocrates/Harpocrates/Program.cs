dfefc54dcf8e899e8c7e767ec8608926b6cf736e
Spencer Pearlman <spencerj521@gmail.com>
2019-08-09 12:24:07 -0400

    Created cheesey intro line to introduce program :p

--- Harpocrates/Harpocrates/Program.cs
+++ Harpocrates/Harpocrates/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+
 
 namespace Harpocrates
 {
@@ -6,7 +8,13 @@ namespace Harpocrates
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Because I am extra, I want the message to print letter by letter
+            string introLine = "Hello, my name is Harpocrates, the gatekeeper of secrets. What secrets would you like to uncover?";
+            for (int i = 0; i < introLine.Length; i++)
+            {
+                Console.Write(introLine[i]);
+                Thread.Sleep(75);
+            }
         }
     }
 }
